b01934ee80e7d27805072064610e5b5cddd37930
Kyle Barnett <kbarnett23@gmail.com>
2019-12-18 22:58:51 -0700

    register user

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,3 +1,35 @@
 const router = require("express").Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const secrets = require("../config/secrets.js");
+
+const Users = require("../users/users-model.js");
+
+router.post("/register", (req, res) => {
+  let user = req.body;
+  const hash = bcrypt.hashSync(user.password, 10);
+  user.password = hash;
+
+  Users.add(user)
+    .then(saved => {
+      const token = genToken(saved)
+      res.status(201).json({ created_user: saved, token: token })
+    })
+    .catch(err => {
+      res.status(500).json(error);
+    })
+})
+
+function genToken(user) {
+  const payload = {
+    userid: user.id,
+    username: user.username
+  }
+  const options = {
+    expiresIn: '1h'
+  }
+  const token = jwt.sign(payload, secrets.jwtSecret, options)
+  return token
+}
 
 module.exports = router;
--- users/users-model.js
+++ users/users-model.js
@@ -1,1 +1,15 @@
 const db = require("../data/dbConfig.js");
+
+module.exports = {
+  add,
+  findById
+}
+
+async function add(user) {
+  const [id] = await db("passportHolder").insert(user)
+  return findById(id)
+}
+
+function findById(id) {
+  return db("passportHolder").where({ id }).first()
+}
