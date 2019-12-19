eb529edafb06f40b703afd291b3ab7fb30d4988e
Kyle Barnett <kbarnett23@gmail.com>
2019-12-19 10:46:05 -0700

    testing users get

--- api/server.js
+++ api/server.js
@@ -13,12 +13,20 @@ server.use(cors());
 
 const authRouter = require("../auth/auth-router.js");
 const restaurantRouter = require("../restaurants/restaurant-router.js");
+const Users = require('../users/users-model.js')
 
 server.use("/api/auth", authRouter);
 server.use("/api/restaurants", restaurantRouter);
 
 server.get('/', (req, res) => {
   res.json({ api: 'Up and running.' })
+})
+
+server.get('/users', (req, res) => {
+  Users.find()
+    .then(results => {
+      res.json(results)
+    })
 })
 
 module.exports = server;
--- users/users-model.js
+++ users/users-model.js
@@ -2,12 +2,17 @@ const db = require("../data/dbConfig.js");
 
 module.exports = {
   add,
+  find,
   findById
 }
 
 async function add(user) {
   const [id] = await db("passportHolder").insert(user)
   return findById(id)
+}
+
+function find() {
+  return db("passportHolder")
 }
 
 function findById(id) {
