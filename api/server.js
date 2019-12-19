ad98f78f70e24b7e83ab5af1bfe3bfe27f5027aa
Kyle Barnett <kbarnett23@gmail.com>
2019-12-18 19:49:02 -0700

    simple server running

---
+++ api/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+const jwt = require('jsonwebtoken');
+const server = express();
+
+server.use(helmet());
+server.use(express.json());
+server.use(cors());
+
+server.get('/', (req, res) => {
+  res.json({ api: 'Up and running.' })
+})
+
+module.exports = server;
